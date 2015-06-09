bd37de1a77d337193b2b415158b5ccb8749cb6fb
adrianalpin <adrian_jilong@hotmail.com>
2015-06-09 14:19:50 +0200

    new layout

---
+++ Visualization/javascripts/bar.js
@@ -0,0 +1,108 @@
+d3.json("data/withNRec.json",function(dataset){
+                        
+                            //var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+                            //               11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+                            
+                            //var h=800;
+                            //var w=dataset.length*10;
+                            //var DD = db.exec('SELECT * FROM withNRec'); 
+                            var barPadding=1;
+                    
+                            var margin = {top: 20, right: 30, bottom: 30, left: 40},
+                            w= 5000 - margin.left - margin.right,
+                            h= 400 - margin.top - margin.bottom;
+    
+                            var cValue = function(d) { return d.conservation;},
+                                color = d3.scale.category10();
+            
+                            var yScale = d3.scale.linear()
+                                .domain([0, d3.max(dataset, function(d) { return d.numRecordings; })])
+                                .range([h, 0]);
+        
+                            var yAxis = d3.svg.axis()
+                                .scale(yScale)
+                                .orient("left");    
+                                                
+                            var svg = d3.select("#bar")
+                                .append("svg")
+                                .attr("width", w + margin.left +      margin.right)
+                                .attr("height", h + margin.top + margin.bottom)
+                                .append("g")
+                                .attr("transform",
+                                        "translate(" + margin.left + "," + margin.top + ")");
+                            
+                            svg.append("g")
+                                .attr("class", "axis")
+                                .attr("transform", "translate(" -30 + ",0)")
+                                .style("opacity",1)
+                                //.style("fill","none");
+                                .call(yAxis)
+                            .append("text")
+                                .attr("class", "label")
+                                .attr("transform", "rotate(-90)")
+                                .attr("y", 6)
+                                .attr("dy", ".71em")
+                                .attr("x",0)
+                                .style("text-anchor", "end")
+                                .text("Nunber of Recordings");
+
+                           
+                          
+                            
+                            svg.selectAll("rect")
+                                .data(dataset)
+                                .data(dataset.sort(function(a, b){return b.numRecordings-                                                               a.numRecordings}))
+                                .enter()
+                                .append("rect")
+                                .attr("x",0)
+                                .attr("y", function(d){
+                                    return yScale(d.numRecordings);
+                                })
+                                .attr("width", w/dataset.length -barPadding)
+                                .attr("x",function(d,i){
+                                    return i* (w/ dataset.length)+15;
+                                })
+                                .attr("height",function(d) {
+                                    return h-yScale(d.numRecordings);
+                                })
+                                .style("fill", function(d) { return color(cValue(d));})
+                                .on("mouseover", function(d) {
+                                tooltip.transition()
+                                    .duration(200)
+                                    .style("opacity", .9);
+                                tooltip.html("<h>"+d.name+"</h>" + "<br/>"  +"<p>"+ "Number of Recordings: " +                                               d.numRecordings+ "<br/>"+"Conservation Status: "+ d.conservation+"</p>")
+                       
+                                    .style("left", (d3.mouse(this)[0]-document.getElementById("bar").scrollLeft) + "px")
+                                    .style("top", (1050 + d3.mouse(this)[1]) + "px");
+                                })
+            
+                                .on("mouseout", function(d) {
+                                tooltip.transition()
+                                    .duration(500)
+                                    .style("opacity", 0);
+                                });
+                        
+                            /*svg.selectAll("text")
+                                .data(dataset)
+                                .enter()
+                                .append("text")
+                                .attr("class", "text")
+                                .text(function(d) {
+                                    return d.numRecordings;
+                                })
+                                .attr("x", function(d, i){
+                                    return i * (w/dataset.length) + ((w/dataset.length-                                               barPadding)/2);
+                                })
+                                .attr("y", function(d){
+                                    return h - (d.numRecordings) + 14;
+                                })
+                                .attr("font-family", "sans-serif")
+                                .attr("font-size", "11px")
+                                .attr("fill", "white")
+                                .attr("text-anchor", "middle");*/
+                   
+                var tooltip = d3.select("#bar").append("div")
+                                .attr("class", "tooltip")
+                                .style("opacity", 0);
+                                
+                    });
---
+++ Visualization/javascripts/scatter.js
@@ -0,0 +1,197 @@
+d3.json("data/withNRec.json",function(dataset){
+    var h=600;
+    var w=500;
+    var padding = 35;
+    var xScale = d3.scale.linear()
+            .domain([0, d3.max(dataset, function(d) { return d.AvgWingspan; })])
+            .range([padding, w-padding]);
+    
+    var yScale = d3.scale.linear()
+            .domain([0, d3.max(dataset, function(d) { return d.AvgLength; })])
+            .range([h-padding, padding]);
+         
+    var rScale = d3.scale.linear()
+            .domain([0, d3.max(dataset, function(d) { return d.AvgWeight; })])
+            .range([4, 35]);
+    
+    var xAxis = d3.svg.axis()
+            .scale(xScale)
+            .orient("bottom");
+    
+    var yAxis = d3.svg.axis()
+            .scale(yScale)
+            .orient("left");
+    
+    var cValue = function(d) { return d.food;},
+        color = d3.scale.category20();
+    
+    var tooltip = d3.select("#scatter").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);    
+        
+    var svg = d3.select("#scatter")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+   
+    svg.selectAll("circle")
+            .data(dataset)
+            .enter()
+            .append("circle")
+            .attr("selected", 1)
+            .style("fill", function(d) { return color(cValue(d));})
+            .style("stroke","white")
+            .style("opacity",0.7)
+        	.attr("id", function(d){
+                return d.food;
+            })
+            .attr("cx", function(d) {
+                return xScale(d.AvgWingspan);
+            })
+            .attr("cy", function(d) {
+                return yScale(d.AvgLength);
+            })
+            .attr("r", function(d){
+                return rScale(d.AvgWeight);
+            })
+            .on("mouseover", function(d) {
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                tooltip.html("<h>"+d.name+"</h>" + "<br/>"  +"<p>"+ "Wingspan: " + d.AvgWingspan + "<br/>" +                                   "Length: " + d.AvgLength + "<br/>" + "Weight: " + d.AvgWeight + "                               <br/>" + "Food: " + d.food+"</p>")
+                       
+                .style("left", (d3.select(this).attr("cx")) + "px")
+                .style("top", (500 + d3.mouse(this)[1]) + "px");
+            })
+            .on("mouseout", function(d) {
+                tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+      });
+    
+    /*svg.selectAll("text")
+            .data(dataset)
+            .enter()
+            .append("text")
+            .text(function(d){
+                return d.name;
+            })
+            .attr("x",function(d){
+              return xScale(d.AvgWingspan);  
+            })
+            .attr("y",function(d){
+              return yScale(d.AvgLength);  
+            })
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "8px")
+            .attr("fill", "red");*/
+    
+    svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0," + (h - padding) + ")")
+            .call(xAxis)
+        .append("text")
+            .attr("class", "label")
+            .attr("x", w-padding)
+            .attr("y", -6)
+            .style("text-anchor", "end")
+            .text("Wingspan (cm)");
+    
+    svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + padding + ",0)")
+            .style("opacity",1)
+            //.style("fill","none");
+            .call(yAxis)
+        .append("text")
+            .attr("class", "label")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .attr("x",-padding)
+            .style("text-anchor", "end")
+            .text("Length (cm)");
+      
+    // draw legend
+    var legend = svg.selectAll(".legend")
+            .data(color.domain())
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(0," +( 200 + i * 20) + ")";       });
+
+    // draw legend colored rectangles
+    legend.append("rect")
+            .attr("name",function(d){
+                return d;
+            })
+            .attr("selected",1)
+            .attr("x", w - 18)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", color)
+            .on("click",click);
+    
+    var arr = [];
+    
+    function click(d){
+        //console.log(d);
+               
+        //console.log(d3.select("rect").attr("name"),d);
+        
+        if(d3.selectAll("[name="+d+"]").attr("selected") == 1){
+        d3.selectAll("[name="+d+"]").transition()
+                                    .style("opacity", 0.2)
+                                    .attr("selected", 0)
+        }else if(d3.selectAll("[name="+d+"]").attr("selected") == 0){
+        d3.selectAll("[name="+d+"]").transition()
+                                    .style("opacity", 1)
+                                    .attr("selected", 1)
+        }
+        
+        if(d3.selectAll("#"+d).attr("selected") == 1){
+            d3.selectAll("#"+d).transition().duration(800)
+                    .style("opacity", 0)
+                    .attr("selected", 0)          
+        
+        if($.inArray(d,arr)==-1){
+                    arr.push(d);
+            }
+            
+        }else if(d3.selectAll("#"+d).attr("selected") == 0){
+            d3.selectAll("#"+d).transition().duration(800)
+                    .attr("selected", 1)
+                    .style("opacity", 0.7)
+        
+        var ind = arr.indexOf(d);
+        if(ind > -1) arr.splice(ind,1);
+            
+        };                    
+        //console.log(arr);
+        output(arr);
+        }
+    
+    var out =[];
+    out.push("scatter");
+    
+    function output(arr){
+        for(i=0; i<arr.length;i++){
+            out.push("food != '"+arr[i]+"'");
+        }
+        console.log(out);
+        runsql(out);
+        
+    }
+    
+    
+        
+    // draw legend text
+    legend.append("text")
+            .attr("x", w - 24)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .text(function(d) { return d;})
+        
+});
+
+   
